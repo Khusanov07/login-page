80b63d6c10330c1ec434aecb78e1c4d4a1df39a7
shohijahon <shohijahonhusanov9@gmail.com>
2021-08-04 14:10:02 +0500

    React file 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const langContent={
+    uz:{
+      first:"Xush kelibsiz!",
+      header:"Kirish",
+      top:"Iltimos email va parolingizni kiriting!",
+      title: "Emailingizni kiriting",
+      parol:"Parolni kiriting",
+      text: "Parolni unutdingizmi?",
+      button:"Kirish"
+    },
+    en:{
+      first:"Welcome!",
+      header:"Login",
+      top:"Please enter your login and password!",
+      title:"Login Email",
+      parol:"Password",
+      text: "Forget password?",
+      button:"Log In"
+    },
+    rus:{
+      first:"Welcome!",
+      header:"Авторизоваться",
+      top:"Введите ваш логин и пароль",
+      title:"Логин",
+      parol:"Пароль",
+      text: "Забыли пароль?",
+      button:"Вход"
+
+    }
+  }
+             
+  
+  const[lang, setlang]=useState("en")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <h1>{langContent[lang].first}</h1> */}
+      
+    {/* {lang} */}
+    <select 
+      defaultValue={lang}
+      onChange={(e)=>setlang(e.target.value)}
+      >
+      
+      <option className="uzb" value="uz">Uzbek</option>
+      <option className="eng" value="en">English</option>
+      <option value="rus">Russian</option>
+      
+    </select>
+    <div className="Login">
+    
+      <h2>{langContent[lang].header}</h2>
+      <p>{langContent[lang].top}</p>
+    <input
+    type="email"
+    placeholder={langContent[lang].title}
+    required
+    />
+    <input  
+    required
+    type="password"
+    placeholder={langContent[lang].parol}
+    /> 
+    <p>{langContent[lang].text}</p>
+    <button>{langContent[lang].button}</button>
+
     </div>
+            
+    </>
   );
 }
 
